9af8f904eaf8c60feaf8efb3e3a9d2aec628b51b
Kevin C. Bimonte <kbimonte@gmail.com>
2020-10-20 15:57:55 -0400

    Skeleton for DatabaseUtil

---
+++ back-end/src/main/java/edu/wpi/cs/calliope/snippetsystem/db/DatabaseUtil.java
@@ -0,0 +1,8 @@
+package edu.wpi.cs.calliope.snippetsystem.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+	// TODO - Create (or steal) database connection class
+}
