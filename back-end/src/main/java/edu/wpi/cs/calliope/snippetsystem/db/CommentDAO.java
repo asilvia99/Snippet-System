8a54a84439d825d5a2226a58954e6072c83694d2
Kevin C. Bimonte <8795118+kcbimonte@users.noreply.github.com>
2020-11-09 11:34:46 -0500

    Comment and CommentDAO should be done.

---
+++ back-end/src/main/java/edu/wpi/cs/calliope/snippetsystem/db/CommentDAO.java
@@ -0,0 +1,116 @@
+package edu.wpi.cs.calliope.snippetsystem.db;
+
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import edu.wpi.cs.calliope.snippetsystem.model.Comment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Gets a comment from the database
+ */
+public class CommentDAO {
+
+    private final LambdaLogger logger;
+    Connection conn;
+
+    final String tblName = "comment";   // Exact capitalization
+
+    /**
+     * Comment Database Access Object
+     */
+    public CommentDAO(LambdaLogger logger) {
+        this.logger = logger;
+        try  {
+            conn = DatabaseUtil.connect(logger);
+        } catch (Exception e) {
+        	logger.log(e.getLocalizedMessage());
+            conn = null;
+        }
+        logger.log("Conn: " + (conn != null));
+    }
+
+    /**
+     * Deletes a comment from the database
+     *
+     * @param id the id of the comment
+     * @return true if the number of comments deleted equals 1
+     * @throws Exception thrown if SQL Exception is triggered
+     */
+    public boolean deleteComment(String id) throws Exception {
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("DELETE From " + tblName + " Where ID=?;");
+            preparedStatement.setString(1, id);
+            int numAffected = preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+            return (numAffected == 1);
+        } catch (SQLException e) {
+            throw new Exception("Failed to delete comment: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Adds a comment to the database
+     *
+     * @param comment the comment to add
+     * @return true if inserted, false if already exists
+     * @throws Exception thrown if SQL Exception is triggered
+     */
+    public boolean addComment(Comment comment) throws Exception {
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM " + tblName + " WHERE ID=?;");
+            preparedStatement.setString(1, comment.getID());
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                resultSet.close();
+                return false;
+            }
+
+            preparedStatement = conn.prepareStatement("INSERT INTO " + tblName + "(ID, S_ID, Text, Start, End) value (?,?,?,?,?)");
+            preparedStatement.setString(1, comment.getID());
+            preparedStatement.setString(2, comment.getS_ID());
+            preparedStatement.setString(3, comment.getText());
+            preparedStatement.setString(4, comment.getStart());
+            preparedStatement.setString(5, comment.getEnd());
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException e) {
+            throw new Exception("Failed to create a comment: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Gets a list of all comments for a specified Snippet ID
+     *
+     * @return a list of Comments
+     * @throws Exception thrown if SQL Exception is triggered
+     */
+    public List<Comment> getAllComments(String s_id) throws Exception {
+        List<Comment> allComments = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("Select * from " + tblName + " where S_ID=?;");
+            preparedStatement.setString(1, s_id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Comment comment = Comment.makeComment(resultSet);
+                allComments.add(comment);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            return allComments;
+
+        } catch (SQLException e) {
+            throw new Exception("Failed in getting all comments for the specified s_id: " + e.getMessage());
+        }
+    }
+
+}
---
+++ back-end/src/main/java/edu/wpi/cs/calliope/snippetsystem/model/Comment.java
@@ -0,0 +1,66 @@
+package edu.wpi.cs.calliope.snippetsystem.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+
+public class Comment {
+
+    private final String ID;
+    private final String S_ID;
+    private final String text;
+    private final String start;
+    private final String end;
+    private final Instant created;
+
+    private Comment(String id, String s_id, String text, String start, String end, Instant created) {
+        this.ID = id;
+        this.S_ID = s_id;
+        this.text = text;
+        this.start = start;
+        this.end = end;
+        this.created = created;
+    }
+
+    public static Comment makeComment(ResultSet resultSet) throws Exception {
+        try {
+            String id = resultSet.getString(1);
+            String s_id = resultSet.getString(2);
+            String text = resultSet.getString(3);
+            String start = resultSet.getString(4);
+            String end = resultSet.getString(5);
+            Instant created = resultSet.getTimestamp(6).toInstant();
+            return new Comment(id, s_id, text, start, end, created);
+        } catch (SQLException e) {
+            throw new Exception("Comment could not be created from Result Set: " + e.getMessage());
+        }
+    }
+
+    public static Comment makeComment(String id, String s_id, String text, String start, String end) {
+        return new Comment(id, s_id, text, start, end, null);
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getS_ID() {
+        return S_ID;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+}
